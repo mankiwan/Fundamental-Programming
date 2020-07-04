932145cd59384370094d18e4e060d1b649a92769
nwvickywan <vicky.wan@price.com.hk>
2020-07-04 20:56:28 +0800

    program update

--- Lesson8/main.js
+++ Lesson8/main.js
@@ -1,15 +1,12 @@
-// declare global variables
-var $clickMe = $("#click-me");
-var $getOrgTable = $("#get-original-table");
-var $getProTable = $("#get-processed-table");
+// Declare global variables
 var rowSize = 10;
 var colSize = 10;
 
-/* 
-Create 2D array that filled with random number
-@param {number} rowSize
-@param {number} colSize
-@return {array} cells - 2D array
+/**
+ * Create 2D array that filled with random number
+ * @param {number} rowSize
+ * @param {number} colSize
+ * @returns {array} cells - 2D array
  */
 function create2dArrRandInt(rowSize, colSize) {
   var cells = [];
@@ -22,25 +19,25 @@ function create2dArrRandInt(rowSize, colSize) {
   return cells;
 }
 
-/* 
-Square each number in a 2D array
-@param {array} arrData - 2D array
-@return {array} sqrArrResult - 2D array 
-*/
-function square2dArrNum(arrData) {
+/**
+ * Square each number in a 2D array
+ * @param {array} orgArrData - 2D array
+ * @returns {array} sqrArrResult - 2D array
+ */
+function square2dArrNum(orgArrData) {
   var sqrArrResult = [];
-  for (var i = 0; i < arrData.length; i++) {
-    var sqrArrUnit = arrData[i].map((x) => Math.pow(x, 2));
+  for (var i = 0; i < orgArrData.length; i++) {
+    var sqrArrUnit = orgArrData[i].map((arrUnit) => Math.pow(arrUnit, 2));
     sqrArrResult.push(sqrArrUnit);
   }
   return sqrArrResult;
 }
 
-/* 
-Make HTML table with 2D array
-@param {array} arrValue - 2D array
-@return {string} table 
-*/
+/**
+ * Make the HTML table from 2D array
+ * @param {array} arrValue - 2D array
+ * @returns {string} table - an HTML table generated from 2D array
+ */
 function makeTableHTML(arrValue) {
   var table = document.createElement("table");
   for (var i = 0; i < arrValue.length; i++) {
@@ -55,46 +52,73 @@ function makeTableHTML(arrValue) {
   return table;
 }
 
-/* 
-Convert table to 2D array
-@param {string} tableOne
-@return {array} arrayFields - 2D array
-*/
-function convertTableTo2dArr(tableOne) {
-  var arrayFields = [];
-  var tableOne = document.querySelector("table");
-  var rows = tableOne.children; // get a collection of "tr" elements
+/**
+ * Convert the table into a 2D array
+ * @param {string} tableData
+ * @returns {array} arrayData - 2D array converted from HTML table
+ */
+function convertTableTo2dArr(tableData) {
+  var arrayData = [];
+  var tableData = document.querySelector("table");
+  var rows = tableData.children; // get a collection of "tr" elements
   for (var i = 0; i < rows.length; i++) {
-    var rowArray = [];
+    var arrayRow = [];
     var cols = rows[i].children; // get a collection of "td" elements
     for (var j = 0; j < cols.length; j++) {
-      rowArray.push(cols[j].innerHTML);
+      arrayRow.push(cols[j].innerHTML);
     }
-    arrayFields.push(rowArray);
+    arrayData.push(arrayRow);
   }
-  return arrayFields;
+  return arrayData;
 }
 
-function lesson8Exe() {
-  // Append original table to target div
-  var orgTable = makeTableHTML(create2dArrRandInt(rowSize, colSize));
-  $getOrgTable.append("<h1>Original Table</h1>");
-  $getOrgTable.append(orgTable);
+// function lesson8Exe() {
+//   // Append original table to target div
+//   var orgTable = makeTableHTML(create2dArrRandInt(rowSize, colSize));
+//   $getOrgTable.append("<h1>Original Table</h1>");
+//   $getOrgTable.append(orgTable);
 
-  // Convert the table into a 2D array
-  var getTable = $getOrgTable.find("table");
-  var extract2dArray = convertTableTo2dArr(getTable);
-  console.log(extract2dArray); // The original, unchanged
+//   // Convert the table into a 2D array
+//   var getTable = $getOrgTable.find("table");
+//   var extract2dArray = convertTableTo2dArr(getTable);
+//   console.log(extract2dArray); // The original, unchanged
 
-  // Store the result - Approach 1: - Update and return a new 2D array
-  var processed2dArray = square2dArrNum(extract2dArray);
-  console.log(processed2dArray); // The processed, changed
+//   // Store the result - Approach 1: - Update and return a new 2D array
+//   var processed2dArray = square2dArrNum(extract2dArray);
+//   console.log(processed2dArray); // The processed, changed
 
-  // Store the result - Approach 2: - Update the original 2D array
-  var proTable = makeTableHTML(processed2dArray);
-  $getProTable.append("<h1>Processed Table</h1>");
-  $getProTable.append(proTable);
-}
+//   // Store the result - Approach 2: - Update the original 2D array
+//   var proTable = makeTableHTML(processed2dArray);
+//   $getProTable.append("<h1>Processed Table</h1>");
+//   $getProTable.append(proTable);
+// }
 
-// button click function
-$clickMe.click(lesson8Exe);
+// button with click to run function
+// $clickMe.click(lesson8Exe);
+
+$(document).ready(function () {
+  // Declare selected DOM elements
+  var $clickMe = $("#click-me");
+  var $getOrgTable = $("#get-original-table");
+  var $getProTable = $("#get-processed-table");
+  $clickMe.click(function () {
+    // Make the original table
+    var orgTable = makeTableHTML(create2dArrRandInt(rowSize, colSize));
+    $getOrgTable.append("<h1>Original Table</h1>");
+    $getOrgTable.append(orgTable);
+
+    // Convert the original table into a 2D array
+    var getTable = $getOrgTable.find("table");
+    var extract2dArray = convertTableTo2dArr(getTable);
+    console.log(extract2dArray); // The original, unchanged
+
+    // Update and return a new 2D array
+    var processed2dArray = square2dArrNum(extract2dArray);
+    console.log(processed2dArray); // The processed, changed
+
+    // Update and return a new table
+    var proTable = makeTableHTML(processed2dArray);
+    $getProTable.append("<h1>Processed Table</h1>");
+    $getProTable.append(proTable);
+  });
+});
